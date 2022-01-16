4498ba4b1c5ad427db873742fe0a321d0ea01862
Tanner Cole <55705685+tcole2cold@users.noreply.github.com>
2022-01-16 18:14:19 -0500

    Add files via upload

---
+++ trailFinder/src/main/java/com/tcole/trailFinder/TrailFinderApplication.java
@@ -0,0 +1,13 @@
+package com.tcole.trailFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailFinderApplication.class, args);
+	}
+
+}
