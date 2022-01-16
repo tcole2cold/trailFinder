b88206f482ca81f0b91bc4ecd757568aed2cea21
Tanner Cole <55705685+tcole2cold@users.noreply.github.com>
2022-01-16 18:26:28 -0500

    Add files via upload

---
+++ src/test/java/com/tcole/trailFinder/TrailFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tcole.trailFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrailFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
